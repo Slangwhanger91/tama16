225cf819c1709eac465efe4553b377fcbc6aef4d
aperte <aperte@ctrlz.info>
2016-06-21 22:30:14 +0200

    testing moving/reading position and setting background color/transparency of window

--- app/src/main/java/com/amitz/androgotchi/MainActivity.java
+++ app/src/main/java/com/amitz/androgotchi/MainActivity.java
@@ -12,24 +12,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         StandOutWindow.closeAll(this, SimpleWindow.class);
-        StandOutWindow.closeAll(this, MultiWindow.class);
-        StandOutWindow.closeAll(this, WidgetsWindow.class);
-
-        // show a MultiWindow, SimpleWindow
-
         StandOutWindow.show(this, SimpleWindow.class, StandOutWindow.DEFAULT_ID);
-        StandOutWindow.show(this, MultiWindow.class, StandOutWindow.DEFAULT_ID);
-        StandOutWindow.show(this, WidgetsWindow.class, StandOutWindow.DEFAULT_ID);
-
-
-
-        // show a MostBasicWindow. It is commented out because it does not
-        // support closing.
-        /*
-        * StandOutWindow.show(this, StandOutMostBasicWindow.class, StandOutWindow.DEFAULT_ID);
-        */
-
-
         finish();
 
     }
--- app/src/main/java/com/amitz/androgotchi/SimpleWindow.java
+++ app/src/main/java/com/amitz/androgotchi/SimpleWindow.java
@@ -1,7 +1,10 @@
 package com.amitz.androgotchi;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import wei.mark.standout.StandOutWindow;
@@ -9,6 +12,8 @@ import wei.mark.standout.constants.StandOutFlags;
 import wei.mark.standout.ui.Window;
 
 public class SimpleWindow extends StandOutWindow {
+	int r, g, b, a; // 'global' per service, not per window
+	int x, y; // same
 
 	@Override
 	public String getAppName() {
@@ -24,7 +29,24 @@ public class SimpleWindow extends StandOutWindow {
 	public void createAndAttachView(int id, FrameLayout frame) {
 		// create a new layout from body.xml
 		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-		inflater.inflate(R.layout.simple, frame, true);
+		View view = inflater.inflate(R.layout.simple, frame, true);
+		Button button = (Button) view.findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Window window = getWindow(0);
+				window.setBackgroundColor(Color.parseColor(String.format("#%02X%02X%02X%02X", a & 0xff, r & 0xff, g & 0xff, b & 0xff)));
+				window.edit().setPosition(x, y);
+				window.edit().commit();
+				// & 0xff to never overflow from max two hex characters in Color string(s)
+				a += 33;
+				x += 10;
+				System.out.printf("BG=#%02X%02X%02X%02X\n", a & 0xff, r & 0xff, g & 0xff, b & 0xff);
+				System.out.println(window.getLayoutParams());
+			}
+
+		});
 	}
 
 	// the window will be centered
